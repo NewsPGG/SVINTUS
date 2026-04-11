c7d93c65486043a3e6bbc684639ebe113dbc4678
Кирилл Симонов <kirillsimonov3@mail.ru>
2026-04-12 02:08:44 +0300

    Add hello world

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
